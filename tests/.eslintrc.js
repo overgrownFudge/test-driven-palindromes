ba7af59b7442574cc98c6927a370ddc70d32ddd6
overgrownFudge <overgrownfudge@gmail.com>
2022-11-14 11:21:09 -0500

    prettier config

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -3,5 +3,11 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:jest/recommended'],
   parserOptions: {
     ecmaVersion: 6,
-  }
+  },
+
+  // prettier config
+  rules: {
+    quotes: ['error', 'single', { avoidEscape: true }], // allowing for single quotes instead of double
+    'comma-dangle': ['error', 'always-multiline'], // change code diff report
+  },
 };
