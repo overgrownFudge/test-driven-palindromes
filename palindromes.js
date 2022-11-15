549bd324d96347fb7aea422ce7aa56785406f55d
overgrownFudge <overgrownfudge@gmail.com>
2022-11-15 10:46:05 -0500

    formatted code with prettier

--- palindromes.js
+++ palindromes.js
@@ -1,3 +1,3 @@
-module.exports = (str) => {
-    return [str]
-}
+module.exports = str => {
+  return [str];
+};
--- tests/palindromes.test.js
+++ tests/palindromes.test.js
@@ -1,13 +1,13 @@
-const palindromes = require('../palindromes')
+const palindromes = require('../palindromes');
 
 describe('palindromes()', () => {
-    it('correctly identifies one-word palindromes', () => {
-        expect(palindromes('madam')).toEqual(['madam']) //1
-        expect(palindromes('racecar')).toEqual(['racecar'])
-    })
-})
+  it('correctly identifies one-word palindromes', () => {
+    expect(palindromes('madam')).toEqual(['madam']); //1
+    expect(palindromes('racecar')).toEqual(['racecar']);
+  });
+});
 
-// 1. Previously, we’ve made equality assertions with toBe(). 
-// However, toBe() does a strict equality check, which would fail here. 
-// The toEqual() assertion method, by contrast, checks for deep object equality. 
+// 1. Previously, we’ve made equality assertions with toBe().
+// However, toBe() does a strict equality check, which would fail here.
+// The toEqual() assertion method, by contrast, checks for deep object equality.
 // So the assertion expect(x).toEqual(['madam']) passes as long as x is an array with the string "madam" as its only entry.
